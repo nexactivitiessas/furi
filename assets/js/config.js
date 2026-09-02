13447da9c49ca4fb8f29623810e51af2849afe1c
tb-asociados <167807905+nexactivitiessas@users.noreply.github.com>
2026-09-02 16:04:58 -0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -10,7 +10,7 @@ window.FURI.config = {
   // Ej: 'https://script.google.com/macros/s/AKfycb.../exec'
   API_URL: 'https://script.google.com/macros/s/AKfycbyXe8DFOA9M4HNjIe18p2SZvqX6I0HFEFGkg-b7q-N99_3qPEvLvlqqrTIsPsrPETxaxw/exec',
 
-  // Cantidad de productos por página en el catálogo
+  // Cantidad de productos por página en el catálogo 
   PRODUCTOS_POR_PAGINA: 12,
 
   // WhatsApp: si Configuracion.WhatsApp está cargado se usa ese; esto es un fallback
