6eddc19282aa95b59f97e23ea53aa572c6ae1ecc
tb-asociados <167807905+nexactivitiessas@users.noreply.github.com>
2026-09-02 16:30:48 -0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -7,7 +7,7 @@
  */
 window.FURI = window.FURI || {};
 window.FURI.config = {
-  // Ej: 'https://script.google.com/macros/s/AKfycb.../exec'
+  // Ej: 'https://script.google.com/macros/s/AKfycb.../exec' 
   API_URL: 'https://script.google.com/macros/s/AKfycbyXe8DFOA9M4HNjIe18p2SZvqX6I0HFEFGkg-b7q-N99_3qPEvLvlqqrTIsPsrPETxaxw/exec',
 
   // Cantidad de productos por página en el catálogo 
